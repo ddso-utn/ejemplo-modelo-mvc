3ebbf816f610ef5a2ce6c16cdbd0a6c9ecca4cb1
longonicolas <nlongo@frba.utn.edu.ar>
2025-04-14 18:12:26 -0300

    definicion capas

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Server } from "./server.js"
+import { MockProductRepository } from "./products/product.repository.js"
+import { ProductController } from "./products/product.controller.js"
+import express from 'express'
+import { config } from "./config.js"
+
+const productRepository = new MockProductRepository()
+const productController = new ProductController(productRepository)
+
+const server = new Server(
+    express(),
+    config.SERVER_PORT,
+    true
+)
+server.setController(ProductController, productController)
+server.configureRoutes()
+server.launch()
---
+++ server.js
@@ -0,0 +1,44 @@
+import { ProductController } from "./megasuper/controllers/controller.js"
+
+export class Server {
+
+    #requiredControllers = [ProductController]
+    #controllers = {}
+    #app
+
+    constructor(app, port = 3000, strict = false) {
+        this.#app = app
+        this.port = port
+        this.strict = strict
+    }
+
+    get app() { return this.#app }
+
+    setController(controllerClass, controller) {
+        this.#controllers[controllerClass.name] = controller
+    }
+
+    getController(controllerClass) {
+        const controller = this.#controllers[controllerClass.name]
+        if (!controller) { throw new Error("Controller missing for the given route.") }
+        return controller
+    }
+
+    configureRoutes() {
+        this.app.get("/products", (req, res) => this.getController(ProductController).findAll(req,res))
+        this.app.get("/users", (req, res) => this.getController(UserController).findAll(req, res))
+    }
+
+    launch() {
+        if (this.strict && !this.#requiredControllers.every(
+            requiredController => Object.keys(this.#controllers).includes(requiredController.name)
+        )) {
+            const missingControllersNames = this.#requiredControllers.filter(
+                c => !Object.keys(this.#controllers).includes(c)
+            ).map(c => c.name)
+            const missingControllersString = missingControllersNames.join(", ") 
+             throw new Error("Controllers are still missing: " + missingControllersNames)
+        }
+        this.app.listen(this.port, () => {console.log("Server running in port " + this.port)})
+    }
+}
